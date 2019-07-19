e749933acacfbe750878680d17fa3892de72e51d
rimduhui <>
2019-07-19 10:48:57 +0900

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div class="text">
+                <pre>
+                    Test
+                    Test
+                    Test
+                    Test
+                </pre>
+            </div>
+        );
+    }
+}
+
+export default App;
